a10389bbd3cf48bfae71898b0b8d45fb0303ad33
Michio Nakagawa <michio.nakagawa@gmail.com>
2016-02-27 17:42:03 +0900

    initail commit

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+DIR=$(cd $(dirname $0) && pwd)
+CONFIG_PATH=~/.config/awesome
+
+# install pro theme
+if [ -d $CONFIG_PATH ]; then
+    rm -rf $CONFIG_PATH.bak
+    mv $CONFIG_PATH/ $CONFIG_PATH.bak
+fi
+git clone https://github.com/barwinko/pro.git $CONFIG_PATH
+
+# install cycle focus
+if [ -d $CONFIG_PATH/cyclefocus ]; then
+    rm -rf $CONFIG_PATH/cyclefocus.bak
+    mv $CONFIG_PATH/cyclefocus $CONFIG_PATH/cyclefocus.bak
+fi
+git clone https://github.com/blueyed/awesome-cyclefocus $CONFIG_PATH/cyclefocus
+
+# install cycle focus
+if [ -d $CONFIG_PATH/lain ]; then
+    rm -rf $CONFIG_PATH/lain.bak
+    mv $CONFIG_PATH/lain $CONFIG_PATH/lain.bak
+fi
+git clone https://github.com/copycat-killer/lain.git $CONFIG_PATH/lain
+
+# install my configurations.
+if [ -f $CONFIG_PATH/rc.lua ]; then
+    rm -f $CONFIG_PATH/rc.lua.bak
+    mv $CONFIG_PATH/rc.lua $CONFIG_PATH/rc.lua.bak
+fi
+ln -s $DIR/config/awesome/rc.lua $CONFIG_PATH
