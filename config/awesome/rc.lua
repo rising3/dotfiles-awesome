420ec06ef8757017f8c3edd01a2ded89cd6084ed
Michio Nakagawa <michio.nakagawa@gmail.com>
2016-02-28 22:43:51 +0900

    fix

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -56,7 +56,7 @@ local exec   = function (s) oldspawn(s, false) end
 local shexec = awful.util.spawn_with_shell
 
 modkey        = "Mod4"
-terminal      = "mlterm"
+terminal      = "gnome-terminal --hide-menubar"
 tmux          = "mlterm -e tmux"
 termax        = "mlterm --geometry 1680x1034+0+22"
 rootterm      = "mlterm -e sudo -i"
